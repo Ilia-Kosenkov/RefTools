92531abf5baef6c8f81f410c2a67a1d0d1cedc13
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-11-17 20:23:10 +0200

    Testing iteration over stack

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using static RefTools.Ref;
 
 namespace Tests
@@ -43,6 +44,48 @@ namespace Tests
                 IsLess(in start, in itt) || AreSame(in start, in itt);
                 itt = ref Dec(in itt)
                 )
+                sum += itt;
+
+            Assert.AreEqual(sum, 120);
+        }
+
+        [Test]
+        public void Test_Inc_Stack()
+        {
+            ReadOnlySpan<ulong> x = stackalloc ulong[]
+            {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+            };
+
+            ref readonly var end = ref x[x.Length - 1];
+            ulong sum = 0;
+
+            for (
+                ref readonly var itt = ref x[0];
+                IsLess(in itt, in end) || AreSame(in itt, in end);
+                itt = ref Inc(in itt)
+            )
+                sum += itt;
+
+            Assert.AreEqual(sum, 120);
+        }
+
+        [Test]
+        public void Test_Dec_Stack()
+        {
+            ReadOnlySpan<ulong> x = stackalloc ulong[]
+            {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+            };
+
+            ref readonly var start = ref x[0];
+            ulong sum = 0;
+
+            for (
+                ref readonly var itt = ref x[x.Length - 1];
+                IsLess(in start, in itt) || AreSame(in start, in itt);
+                itt = ref Dec(in itt)
+            )
                 sum += itt;
 
             Assert.AreEqual(sum, 120);
